49108f6f5c503f3e0836fcdf94dbdcd2b60b3b03
SizzlinSeal <eduard.teale@gmail.com>
2021-03-08 13:35:59 -0800

    Initial commit

---
+++ include/devices.h
@@ -0,0 +1,18 @@
+#include "main.h"
+// Vex devices
+extern pros::Controller master;
+extern pros::Controller partner;
+extern pros::Motor leftMotorA;
+extern pros::Motor leftMotorB;
+extern pros::Motor rightMotorA;
+extern pros::Motor rightMotorB;
+extern okapi::MotorGroup leftDriveSmart;
+extern okapi::MotorGroup rightDriveSmart;
+extern pros::Motor leftArmMotor;
+extern pros::Motor rightArmMotor;
+extern pros::Motor leftUpwards;
+extern pros::Motor rightUpwards;
+extern pros::ADIAnalogIn leftBumper;
+extern pros::ADIAnalogIn rightBumper;
+extern pros::ADIAnalogIn limitSwitchC;
+extern pros::ADIAccelerometer accel2G;
---
+++ src/robot-config.cpp
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "devices.h"
+
+void initialize() {
+	// initialize Controller
+	pros::Controller master (CONTROLLER_MASTER);
+	pros::Controller partner (CONTROLLER_PARTNER);
+
+	// initialize drivetrain motors
+	pros::Motor leftMotorA_initializer (21, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+	pros::Motor leftMotorB (9, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+	pros::Motor rightMotorA (1, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+	pros::Motor rightMotorB (8, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+	// initialize motor groups
+	okapi::MotorGroup leftDriveSmart (pros::Motor leftMotorA, pros::Motor leftMotorB);
+	okapi::MotorGroup rightDriveSmart (pros::Motor rightMotorA, pros::Motor rightMotorB);
+	// intake motors
+	pros::Motor leftArmMotor (2, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+	pros::Motor rightArmMotor (20, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+	// outake motors
+	pros::Motor leftUpwards (4, MOTOR_GEARSET_6, false, MOTOR_ENCODER_DEGREES);
+	pros::Motor rightUpwards (5, MOTOR_GEARSET_6, true, MOTOR_ENCODER_DEGREES);
+	// initialize sensors
+  pros::ADIAnalogIn leftBumper ('A');
+  pros::ADIAnalogIn rightBumper ('B');
+  pros::ADIAnalogIn limitSwitchC ('C');
+  pros::ADIAccelerometer accel2G ('D');
+}
