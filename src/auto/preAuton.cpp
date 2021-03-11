c411e2b37c99e0e9fee50232b7f43cbffb0267d0
SizzlinSeal <eduard.teale@gmail.com>
2021-03-11 09:10:25 -0800

    modify pre-auton to allow release of new hood release design

--- src/auto/preAuton.cpp
+++ src/auto/preAuton.cpp
@@ -2,11 +2,7 @@
 #include "devices.h"
 
 void pre_Auton() {
-  // relase front hood section
-  outakeControl(-200);
-  pros::delay(400);
-  outakeStop();
-  // release base hood section
+  // release hood
   intakeControl(-50);
   pros::delay(400);
   intakeStop();
